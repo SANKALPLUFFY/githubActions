a5870ab241044f88b399e4b6a1f2ac9003597c65
SANKALPLUFFY <67747630+SANKALPLUFFY@users.noreply.github.com>
2021-08-18 11:11:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const http = require('http');
 
+	const server = http.createServer((req, res) => {
+	    if (req.url === '/') {
+	        res.end('<h1>Hello, World</h1>');
+	    }
+	});
+
+	const PORT = process.env.PORT || 5000;
